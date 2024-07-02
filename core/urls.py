ad53bcf7464f324bce340f0d8b2b53a9f8d95de3
unknown <you@example.com>
2024-07-02 16:19:00 -0300

    dia 2 karina

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,15 @@
 
 from django.urls import path
 from .views import pesquisa_view, sucesso_view
+from .views import relatorio
+from .views import relatorio, export_excel
+
+
 
 urlpatterns = [
     path('', pesquisa_view, name='pesquisa'),  # Define a URL base como a view de pesquisa
     path('sucesso/', sucesso_view, name='sucesso'),
+    path('relatorio/', relatorio, name='relatorio'),
+    path('export_excel/', export_excel, name='export_excel'),
+
 ]
--- core/views.py
+++ core/views.py
@@ -5,6 +5,47 @@ from django.shortcuts import render
 
 from django.shortcuts import render, redirect
 from .forms import PesquisaForm
+from .models import Pesquisa
+from openpyxl import Workbook
+from django.http import HttpResponse
+
+
+
+def export_excel(request):
+    # Criar um Workbook
+    wb = Workbook()
+    ws = wb.active
+    ws.title = "Pesquisa Nilópolis - JULHO 2024"
+    
+    # Cabeçalhos
+    headers = [
+        'Data', 'Pesquisadora', 'Bairro', 'Sexo', 'Idade',
+        'Voto Espontâneo', 'Voto Atual', 'Voto dos Nomes', 'Avaliação da Prefeitura'
+    ]
+    ws.append(headers)
+    
+    # Adicionar dados
+    pesquisas = Pesquisa.objects.all()
+    for pesquisa in pesquisas:
+        ws.append([
+            pesquisa.data,
+            pesquisa.pesquisadora,
+            pesquisa.bairro,
+            pesquisa.get_sexo_display(),
+            pesquisa.get_idade_display(),
+            pesquisa.voto_espontaneo,
+            pesquisa.get_voto_atual_display(),
+            pesquisa.get_voto_dos_nomes_display(),
+            pesquisa.get_avaliacao_prefeitura_display()
+        ])
+    
+    # Criar uma resposta HTTP com o arquivo Excel
+    response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    response['Content-Disposition'] = 'attachment; filename=pesquisa_nilopolis_julho_2024.xlsx'
+    
+    wb.save(response)
+    return response
+
 
 def pesquisa_view(request):
     if request.method == 'POST':
@@ -18,3 +59,13 @@ def pesquisa_view(request):
 
 def sucesso_view(request):
     return render(request, 'sucesso.html')
+
+
+def relatorio(request):
+    pesquisas = Pesquisa.objects.all()
+    
+    context = {
+        'pesquisas': pesquisas
+    }
+    
+    return render(request, 'relatorio.html', context)
